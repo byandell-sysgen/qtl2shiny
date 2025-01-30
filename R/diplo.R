f27ae97d6d039c22bd8c801d78900d87dbbea4a1
byandell <byandell@wisc.edu>
2025-01-30 13:37:56 -0600

    refactor project

--- R/diplo.R
+++ R/diplo.R
@@ -17,70 +17,72 @@
 diploServer <- function(id, win_par, phe_mx, cov_df, K_chr, analyses_df,
                        project_info, allele_info) {
   shiny::moduleServer(id, function(input, output, session) {
-  ns <- session$ns
-
-  chr_pos <- shiny::reactive({
-    make_chr_pos(win_par$chr_id, 
-                 win_par$peak_Mbp, 
-                 win_par$window_Mbp)
+    ns <- session$ns
+    
+    chr_pos <- shiny::reactive({
+      make_chr_pos(win_par$chr_id, 
+                   win_par$peak_Mbp, 
+                   win_par$window_Mbp)
+    })
+    
+    ## Probs object for allele pair diplotypes.
+    pairprobs_obj <- shinyPairProbs("pairprobs", win_par, project_info)
+    
+    snp_action <- shiny::reactive({input$snp_action})
+    
+    ## SNP Association
+    patterns <- shinySNPSetup("snp_setup", input, win_par, phe_mx, cov_df, K_chr,
+                              analyses_df, project_info, allele_info, snp_action)
+    
+    shinyPattern("dip_pat", input, chr_pos, win_par, phe_mx, cov_df, pairprobs_obj, K_chr,
+                 analyses_df, patterns, project_info, allele_info, snp_action)
+    
+    output$allele_names <- shiny::renderText({
+      shiny::req(allele_info())
+      paste(allele_info()$code, allele_info()$shortname, sep = "=", collapse = ", ")
+    })
+    
+    output$dip_input <- shiny::renderUI({
+      switch(shiny::req(input$button),
+             "Genome Scans"    = shinyPatternUI(ns("dip_pat")),
+             "SNP Association" =,
+             "Allele Pattern"  = shinySNPSetupUI(ns("snp_setup")))
+    })
+    output$dip_output <- shiny::renderUI({
+      switch(shiny::req(input$button),
+             "Genome Scans"    = shinyPatternOutput(ns("dip_pat")),
+             "SNP Association" = ,
+             "Allele Pattern"  = shinySNPSetupOutput(ns("snp_setup")))
+    })
+    output$radio <- shiny::renderUI({
+      shiny::radioButtons(ns("button"), "",
+                          c("SNP Association","Allele Pattern","Genome Scans"),
+                          input$button)
+    })
+    output$select <- shiny::renderUI({
+      shiny::selectInput(ns("snp_action"), "",
+                         c("add+dom","additive","non-add",
+                           "recessive","dominant"),
+                         input$select)
+    })
+    output$sex_type <- shiny::renderUI({
+      choices <- c("A","I","F","M","all")
+      if(ncol(shiny::req(phe_mx())) > 1 | shiny::req(input$button) == "Genome Scans") {
+        choices <- choices[1:4]
+      }
+      shiny::radioButtons(ns("sex_type"), "Sex:",
+                          choices,
+                          input$sex_type, inline = TRUE)
+    })
+    output$project <- shiny::renderUI({
+      shiny::strong(shiny::req(paste("Project:",
+                                     project_info()$project,
+                                     "\n")))
+    })
   })
-
-  ## Probs object for allele pair diplotypes.
-  pairprobs_obj <- shinyPairProbs("pairprobs", win_par, project_info)
-
-  snp_action <- shiny::reactive({input$snp_action})
-  
-  ## SNP Association
-  patterns <- shinySNPSetup("snp_setup", input, win_par, phe_mx, cov_df, K_chr,
-                            analyses_df, project_info, allele_info, snp_action)
-  
-  shinyPattern("dip_pat", input, chr_pos, win_par, phe_mx, cov_df, pairprobs_obj, K_chr,
-               analyses_df, patterns, project_info, allele_info, snp_action)
-  
-  output$allele_names <- shiny::renderText({
-    shiny::req(allele_info())
-    paste(allele_info()$code, allele_info()$shortname, sep = "=", collapse = ", ")
-  })
-
-  output$dip_input <- shiny::renderUI({
-    switch(shiny::req(input$button),
-           "Genome Scans"    = shinyPatternUI(ns("dip_pat")),
-           "SNP Association" =,
-           "Allele Pattern"  = shinySNPSetupUI(ns("snp_setup")))
-  })
-  output$dip_output <- shiny::renderUI({
-    switch(shiny::req(input$button),
-           "Genome Scans"    = shinyPatternOutput(ns("dip_pat")),
-           "SNP Association" = ,
-           "Allele Pattern"  = shinySNPSetupOutput(ns("snp_setup")))
-  })
-  output$radio <- shiny::renderUI({
-    shiny::radioButtons(ns("button"), "",
-                 c("SNP Association","Allele Pattern","Genome Scans"),
-                 input$button)
-  })
-  output$select <- shiny::renderUI({
-    shiny::selectInput(ns("snp_action"), "",
-                c("add+dom","additive","non-add",
-                  "recessive","dominant"),
-                input$select)
-  })
-  output$sex_type <- shiny::renderUI({
-    choices <- c("A","I","F","M","all")
-    if(ncol(shiny::req(phe_mx())) > 1 | shiny::req(input$button) == "Genome Scans") {
-      choices <- choices[1:4]
-    }
-    shiny::radioButtons(ns("sex_type"), "Sex:",
-                        choices,
-                        input$sex_type, inline = TRUE)
-  })
-  output$project <- shiny::renderUI({
-    shiny::strong(shiny::req(paste("Project:",
-                                   project_info()$project,
-                                   "\n")))
-  })
-})
 }
+#' @export
+#' @rdname diploServer
 diploUI <- function(id) {
   ns <- shiny::NS(id)
   shiny::tagList(
--- R/project.R
+++ R/project.R
@@ -1,6 +1,6 @@
-#' Shiny project selection
+#' Shiny project module
 #'
-#' Shiny module for selection of project, with interface \code{shinyProjectUI}.
+#' Shiny module for selection of project, with interface \code{projectUI}.
 #'
 #' @param id identifier for shiny reactive
 #' @param projects_info reactive project info data frame
@@ -14,38 +14,39 @@
 #' @importFrom shiny isTruthy moduleServer NS renderUI selectInput uiOutput
 #' @importFrom dplyr distinct filter
 #' @importFrom rlang .data
-#' 
-shinyProject <- function(id, projects_info) {
+projectServer <- function(id, projects_info) {
   shiny::moduleServer(id, function(input, output, session) {
-  ns <- session$ns
-  
-  output$project <- shiny::renderUI({
-    shiny::req(projects_info())
-    choices <- unique(projects_info()$project)
-    if(is.null(selected <- input$project)) {
-      selected <- choices[1]
-    }
-    shiny::selectInput(ns("project"), "Project", choices, selected)
+    ns <- session$ns
+    
+    output$project <- shiny::renderUI({
+      shiny::req(projects_info())
+      choices <- unique(projects_info()$project)
+      if(is.null(selected <- input$project)) {
+        selected <- choices[1]
+      }
+      shiny::selectInput(ns("project"), "Project", choices, selected)
+    })
+    
+    shiny::reactive({
+      shiny::req(projects_info())
+      project_id <- NULL
+      if(shiny::isTruthy(input$project)) {
+        project_id <- input$project
+      }
+      if(is.null(project_id)) {
+        project_id <- projects_info()$project[1]
+      }
+      dplyr::distinct(
+        dplyr::filter(
+          projects_info(),
+          .data$project == project_id),
+        .data$project, .keep_all = TRUE)
+    })
   })
-  
-  shiny::reactive({
-    shiny::req(projects_info())
-    project_id <- NULL
-    if(shiny::isTruthy(input$project)) {
-      project_id <- input$project
-    }
-    if(is.null(project_id)) {
-      project_id <- projects_info()$project[1]
-    }
-    dplyr::distinct(
-      dplyr::filter(
-        projects_info(),
-        .data$project == project_id),
-      .data$project, .keep_all = TRUE)
-  })
-})
 }
-shinyProjectUI <- function(id) {
+#' @export
+#' @rdname projectServer
+projectUI <- function(id) {
   ns <- shiny::NS(id)
   shiny::uiOutput(ns("project"))
 }
