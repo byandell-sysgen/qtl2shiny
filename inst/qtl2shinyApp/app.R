d03c2bb5f722f6867b2309db39faed782ce89882
byandell <byandell@wisc.edu>
2025-02-01 09:09:20 -0600

    rename qtl2shinyServer as mainServer

--- inst/qtl2shinyApp/app.R
+++ inst/qtl2shinyApp/app.R
@@ -3,11 +3,11 @@
 projects <- read.csv("qtl2shinyData/projects.csv", stringsAsFactors = FALSE)
 ui <- shinydashboard::dashboardPage(skin="red",
   shinydashboard::dashboardHeader(title = "qtl2shiny"),
-  shinydashboard::dashboardSidebar(qtl2shiny::qtl2shinyInput("qtl2shiny")),
-  shinydashboard::dashboardBody(qtl2shiny::qtl2shinyOutput("qtl2shiny"))
+  shinydashboard::dashboardSidebar(qtl2shiny::mainInput("qtl2shiny")),
+  shinydashboard::dashboardBody(qtl2shiny::mainOutput("qtl2shiny"))
 )
 server <- function(input, output, session) {
-  qtl2shiny::qtl2shinyServer("qtl2shiny", projects)
+  qtl2shiny::mainServer("qtl2shiny", projects)
   # Allow reconnect with Shiny Server.
   session$allowReconnect(TRUE)
 }
